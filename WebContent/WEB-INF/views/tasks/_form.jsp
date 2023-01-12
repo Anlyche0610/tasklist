296acddee55e1cb33c25f2a5645b4f528d3fbb28
anna.ootani <anlyche0610@icloud.com>
2023-01-12 12:04:13 +0900

    Index/NewServletの修正

---
+++ WebContent/WEB-INF/views/tasks/_form.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<label for="task">タスク内容</label><br />
+<input type="text" name="task" id="task" value="${task.content}" />
+<br /><br />
+
+<input type="hidden" name="_token" value="${_token}"/>
+<button type="submit">登録</button>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>Insert title here</title>
+    </head>
+    <body>
+
+    </body>
+</html>
--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -34,10 +35,14 @@ public class IndexServlet extends HttpServlet {
         EntityManager em = DBUtil.createEntityManager();
 
         List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
-        //レスポンスのライターを呼び出してそこに値を渡す
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
 
         em.close();
+
+        request.setAttribute("tasks", tasks);//①データの名前②登録するデータ リクエストスコープへ
+
+        //indexjspを呼び出して、forwardで渡す
+      RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+      rd.forward(request, response);
     }
 
 }
